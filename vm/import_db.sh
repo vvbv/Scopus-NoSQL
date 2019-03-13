66da0db9fca8e8ce774323819acdb028dcc6ec46
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-13 00:54:37 -0500

    .

--- vm/import_db.sh
+++ vm/import_db.sh
@@ -3,4 +3,5 @@
 mongod --fork --logpath /var/log/mongodb.log --port 27017
 cd /data
 mongoimport --db proyecto_nosql --collection articles --file mongo_dataset.json --port 27017
-/bin/bash
+cd /
+/mongo --port 27017 0.0.0.0/proyecto_nosql
