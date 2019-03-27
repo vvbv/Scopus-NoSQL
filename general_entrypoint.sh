000844146c4fd671d511ef018556b9e8bf49a108
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-27 17:48:09 -0500

    pass removed

--- general_entrypoint.sh
+++ general_entrypoint.sh
@@ -4,6 +4,6 @@ mongod --fork --logpath /var/log/mongodb.log --port 27017
 mongoimport --db proyecto_nosql --collection articles --file /dataset/mongo_dataset.json --port 27017
 export DBA_PASSWORD=root 
 /bin/bash /entrypoint/virtuoso_entrypoint.sh 
-virtuoso-f
-isql 127.0.0.1:1111 dba root < /dataset/graph.rq
+virtuoso-t
+isql 127.0.0.1:1111 < /dataset/graph.rq
 /bin/bash
