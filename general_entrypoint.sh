f25c2ef70651d5520edb0f705cb99eab96e560c7
vvbv <jeison.cardona@correounivalle.edu.co>
2019-03-27 19:25:22 -0500

    Corrección de mensaje

--- general_entrypoint.sh
+++ general_entrypoint.sh
@@ -5,6 +5,6 @@ mongoimport --db proyecto_nosql --collection articles --file /dataset/mongo_data
 export DBA_PASSWORD=root 
 /bin/bash /entrypoint/virtuoso_entrypoint.sh 
 virtuoso-t +wait
-echo "Iniciando carga de datos a virtuoso, puede tardar unos minutos"
+echo "Iniciando carga de datos a virtuoso, esta operación puede tardar unos minutos."
 isql 127.0.0.1:1111 dba root < /dataset/graph.rq > /dev/null
 /bin/bash
