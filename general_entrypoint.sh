06dbaffa1079b9effed61612a502be9dc47d1808
vvbv <jeison.cardona@correounivalle.edu.co>
2019-04-12 00:11:41 -0500

    short

--- general_entrypoint.sh
+++ general_entrypoint.sh
@@ -3,11 +3,11 @@ echo "[$(date +"%T")][Mongo]: Iniciando servidor."
 mongod --fork --logpath /var/log/mongodb.log --port 27017 > /dev/null
 echo "[$(date +"%T")][Mongo]: Servidor iniciado."
 echo "[$(date +"%T")][Mongo]: Iniciando importación de documentos."
-mongoimport --db proyecto_nosql --collection articles --file /dataset/mongo_dataset.json --port 27017 > /dev/null
+mongoimport --db proyecto_nosql --collection articles --file /dataset/mongo_dataset.json --port 27017 2> /dev/null
 echo "[$(date +"%T")][Mongo]: Documentos importados."
 export DBA_PASSWORD=root 
 echo "[$(date +"%T")][Virtuoso]: Configurando servidor."
-/bin/bash /entrypoint/virtuoso_entrypoint.sh > /dev/null
+/bin/bash /entrypoint/virtuoso_entrypoint.sh 2> /dev/null
 echo "[$(date +"%T")][Virtuoso]: Servidor configurado."
 echo "[$(date +"%T")][Virtuoso]: Iniciando servidor."
 virtuoso-t +wait > /dev/null
